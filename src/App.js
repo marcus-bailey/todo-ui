3b2c16ece619f60052da3683fe45d3937c6fde8a
Marcus <mojave27@gmail.com>
2021-02-21 06:21:16 -0700

    try retrieving value from function.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
+  // const value = 'World';
+  let { value } = await( await fetch(`/api/message`)).json();
+  // document.querySelector('#name').textContent = text;
   return <div>Hello {value}</div>;
 }
 
