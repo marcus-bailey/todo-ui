2b274a6646b0966000cfba0f47d9c1357b9be932
Marcus <mojave27@gmail.com>
2021-02-21 12:13:47 -0700

    added axios.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from 'react'
+import axios from 'axios'
 
 const retrieve = async () => {
-  let { value } = await( await fetch(`/api/message`)).json()
-  console.log(value)
-  return value
+  // let { value } = await( await fetch(`/api/message`)).json()
+  const url = '/api/message'
+  return axios
+    .get(url)
+    .then(function(response) {
+      console.log(response)
+      return response
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(`[api error]: ${error}`);
+      return [];
+    })
+  // console.log(value)
+  // return value
 }
 
 function App() {
-  // const value = 'World';
   const value = retrieve()
   return <div>Hello {value}</div>;
 }
